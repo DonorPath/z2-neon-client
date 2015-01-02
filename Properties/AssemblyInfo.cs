b2f481f3976b8b426c9585230b319449281e0c59
hello-consumer <joe.wendell.johnson@gmail.com>
2015-01-02 14:39:35 -0600

    Updated assembly info with publisher data

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Neon")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Z2Systems Neon Client")]
+[assembly: AssemblyDescription(".NET library for working with Z2 System's Neon Web Service API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DonorPath, Inc.")]
 [assembly: AssemblyProduct("Neon")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
