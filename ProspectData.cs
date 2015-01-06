45f5c2a4f4f9529eb9177313f45d63a71c848bca
hello-consumer <joe.wendell.johnson@gmail.com>
2015-01-05 18:28:24 -0600

    Added missing ProspectData file

---
+++ ProspectData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Z2Systems.Neon
+{
+    public class ProspectData
+    {
+        public string AskAmount { get; set; }
+
+        public string AskDate { get; set; }
+
+        public string Campaign { get; set; }
+
+        public string CampaignId { get; set; }
+
+        public string Created { get; set; }
+
+        public string CustomStatus { get; set; }
+
+        public string Note { get; set; }
+
+        public string PledgeAmount { get; set; }
+
+        public string PledgeDate { get; set; }
+
+        public string Purpose { get; set; }
+
+        public string SocialFundraiser { get; set; }
+
+        public string Solicitor { get; set; }
+
+        public string Status { get; set; }
+
+        public string SystemUser { get; set; }
+
+        public string Updated { get; set; }
+    }
+}
